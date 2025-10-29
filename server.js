7582fec73481c47d2c633df95dcca7a32aa8ccd1
DigitalKshetram <saisandeep270898@gmail.com>
2025-10-29 13:31:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // Your Razorpay Keys
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID || 'rzp_live_JpStg2YIkRVnrI',
+  key_id: process.env.RAZORPAY_KEY_ID || 'rzp_live_RYqgXJKgSDEl7t',
   key_secret: process.env.RAZORPAY_KEY_SECRET || 'YOUR_RAZORPAY_SECRET_HERE'
 });
 
